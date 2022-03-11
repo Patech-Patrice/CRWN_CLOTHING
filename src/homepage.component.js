083456fe9b6b4c309d1385167bb9fb0dff3dc58c
Patech_Patrice <patricedrayton86@gmail.com>
2022-03-11 15:27:11 -0500

    added saas styling to homepage component

--- src/homepage.component.js
+++ src/homepage.component.js
@@ -1,5 +1,8 @@
 import React from 'react';
 
+//import newly created saas file here
+import './homepage.styles.scss';
+
 const HomePage = () => (
     <div className='homepage'>
         <div className='directory-menu'>
