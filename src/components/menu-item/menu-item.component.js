fe68e817a640eebe69f78d1975cba298fa94f383
Patech_Patrice <patricedrayton86@gmail.com>
2022-03-11 16:42:35 -0500

    added hover styling to menu items

--- src/components/menu-item/menu-item.component.js
+++ src/components/menu-item/menu-item.component.js
@@ -2,12 +2,15 @@ import React from 'react';
 import './menu-item.styles.scss';
 
 const MenuItem = ({ title, imageUrl, size }) => ( //destructed prop to dynamically generate title
-<div  style={{
-    backgroundImage: `url(${imageUrl})`
-}}
-className={`${size} menu-item`}>
-    <div className='content'>
-        <h1 className='title'>{title}</h1>
+<div  
+//if there is no size value, nothing will render. If it is present, add it to our class name. Use string interpolation to achieve this.
+        className={`${size} menu-item`}
+        >   
+        <div className='background-image'  style={{
+            backgroundImage: `url(${imageUrl})`
+            }} />
+        <div className='content'>
+        <h1 className='title'>{title.toUpperCase()}</h1>
         <span className='subtitle'>SHOP NOW</span>    
     </div>
 </div>
